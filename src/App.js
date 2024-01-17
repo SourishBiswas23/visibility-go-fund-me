f2b9e64fd532e090c3f0e656441c1794c04a5f0b
SourishBiswas23 <sourish135@gmail.com>
2024-01-17 23:50:57 +0530

    Completed the navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import "./components/styles/theme.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Login}></Route>
+        <Route path="/signUp" Component={SignUp}></Route>
+        <Route path="/forgotPassword" Component={ForgotPassword}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/auth/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../common/Navbar";
+
+function Login() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div>This is the login page</div>
+    </>
+  );
+}
+
+export default Login;
