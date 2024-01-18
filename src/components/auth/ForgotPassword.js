e51a97c2002263e85ba75b0e330e20fe1785d661
SourishBiswas23 <sourish135@gmail.com>
2024-01-18 22:45:10 +0530

    Created the forgot password screen

--- src/components/auth/ForgotPassword.js
+++ src/components/auth/ForgotPassword.js
@@ -1,7 +1,36 @@
 import React from "react";
+import Navbar from "../common/Navbar";
+import Footer from "../common/Footer";
 
 function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  return (
+    <div className="page">
+      <Navbar></Navbar>
+      <div className="content">
+        <div className="heading">Forgot Password</div>
+        <div>
+          Remembered your password? <span className="sign-up-link">Login</span>
+        </div>
+        <div className="input-fields">
+          <input
+            type="text"
+            className="single-input-field"
+            placeholder="Email"
+          />
+          <div className="forgot-password-section"></div>
+          <div className="button">
+            <div className="button-text">Send Password Reset Mail</div>
+          </div>
+          <div className="legal-conditions">
+            By continuing, you accept Change.org's
+            <span className="terms-of-service"> Terms of Service</span> and
+            <span className="privacy-policy"> Privacy Policy </span>.
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default ForgotPassword;
