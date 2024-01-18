a7e13a36efa9fce850e3896d0fd3f030e6347b74
SourishBiswas23 <sourish135@gmail.com>
2024-01-18 22:40:39 +0530

    Added the signup page

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Navbar from "../common/Navbar";
+import Footer from "../common/Footer";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="page">
+      <Navbar></Navbar>
+      <div className="content">
+        <div className="heading">Sign Up</div>
+        <div>
+          Already have an account? <span className="sign-up-link">Login</span>
+        </div>
+        <div className="input-fields">
+          <input type="text" className="input-top" placeholder="User Name" />
+          <input type="text" className="input-middle" placeholder="Email" />
+          <input type="text" className="input-bottom" placeholder="Password" />
+          <div className="forgot-password-section"></div>
+          <div className="button">
+            <div className="button-text">Sign Up</div>
+          </div>
+          <div className="legal-conditions">
+            By signing in or continuing, you accept Change.org's
+            <span className="terms-of-service"> Terms of Service</span> and
+            <span className="privacy-policy"> Privacy Policy </span>.
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default SignUp;
