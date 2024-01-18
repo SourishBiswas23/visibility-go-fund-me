4d362a01a914fac70a9d4d3a7709ed540e9a6372
SourishBiswas23 <sourish135@gmail.com>
2024-01-18 10:00:33 +0530

    Made the login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,12 +1,38 @@
 import React from "react";
 import Navbar from "../common/Navbar";
+import Footer from "../common/Footer";
 
 function Login() {
   return (
-    <>
+    <div className="login-page">
       <Navbar></Navbar>
-      <div>This is the login page</div>
-    </>
+      <div className="login-content">
+        <div className="login-heading">Log in</div>
+        <div>
+          Don't have an account? <span className="sign-up-link">Sign Up</span>
+        </div>
+        <div className="login-input-fields">
+          <input type="text" className="login-input-top" placeholder="Email" />
+          <input
+            type="text"
+            className="login-input-bottom"
+            placeholder="Password"
+          />
+          <div className="forgot-password-section">
+            <div className="forgot-password-text">Forgot Password?</div>
+          </div>
+          <div className="login-button">
+            <div className="login-button-text">Login</div>
+          </div>
+          <div className="login-legal-conditions">
+            By logging in or continuing, you accept Change.org's
+            <span className="terms-of-service"> Terms of Service</span> and
+            <span className="privacy-policy"> Privacy Policy </span>.
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
   );
 }
 
